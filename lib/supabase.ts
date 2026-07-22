08d5f9a9c55ed7d509263446ecad640b29586345
Nurdoolot11 <yryskeldiydyrysov3@gmai.com>
2026-07-22 15:38:38 +0600

    Fix supabase build

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,6 @@
-import { createClient } from '@supabase/supabase-js';
+import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || 'https://ezzmhrflnivuycgocjdc.supabase.co'
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || 'sb_publishable_h4JPfZBitmY3oY64m28T-A_1JmFibt3'
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export const supabase = createClient(supabaseUrl, supabaseAnonKey)
